475b0a10303444015cc29683926e2ac696c858f9
Trevor <trevorfehrman@gmail.com>
2018-09-06 14:51:05 -0700

    instructions seed

--- db/seeds/instructions.js
+++ db/seeds/instructions.js
@@ -5,9 +5,12 @@ exports.seed = function(knex, Promise) {
 		.then(function() {
 			// Inserts seed entries
 			return knex("instructions").insert([
-				{ recipe_id: 1, step: 1 },
-				{ recipe_id: 1, step: 1 },
-				{ recipe_id: 1, step: 1 },
+				{ recipe_id: 1, step: 1, description: "Sear Ahi" },
+				{ recipe_id: 1, step: 2, description: "Scorch Tortillas" },
+				{ recipe_id: 1, step: 3, description: "Assemble" },
+				{ recipe_id: 2, step: 1, description: "Make Dough" },
+				{ recipe_id: 2, step: 1, description: "Make Tomato Sauce" },
+				{ recipe_id: 2, step: 1, description: "Assemble and Bake" },
 			]);
 		});
 };
